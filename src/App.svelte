7ef81ba4867114b177cb889e75bbc77c724b15a1
carrenos355@gmail.com <carrenos355@gmail.com>
2024-05-10 10:41:54 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+	
+</script>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-5">
+
+			<div class="card">
+				<div class="card-header">Empleados</div>
+				<div class="card-body">
+					<form>
+						<div class="mb-3">
+							<label for="" class="form-label">ID</label>
+							<input type="text" class="form-control form-control-sm" name="" id=""
+								aria-describedby="helpId" placeholder="" />
+
+						</div>
+						<div class="mb-3">
+							<label for="" class="form-label">Nombre</label>
+							<input type="text" class="form-control form-control-sm" name="" id=""
+								aria-describedby="helpId" placeholder="" />
+
+						</div>
+						<div class="mb-3">
+							<label for="" class="form-label">Correo</label>
+							<input type="text" class="form-control form-control-sm" name="" id=""
+								aria-describedby="helpId" placeholder="" />
+
+						</div>
+						<div class="mb-3">
+							<label for="" class="form-label">Acciones</label>
+							<input type="text" class="form-control form-control-sm" name="" id=""
+								aria-describedby="helpId" placeholder="" />
+
+						</div>
+						<div class="btn-group" role="group" aria-label="Button group name">
+							<button type="button" class="btn btn-primary">
+								Agregar Empleados
+							</button>
+							
+							&nbsp;
+							&nbsp;
+
+							<button type="button" class="btn btn-primary">
+								Actualizar
+							</button>
+
+						</div>
+
+					</form>
+				</div>
+			</div>
+
+
+			<div class="table-responsive">
+				<table class="table table-primary">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Nombre</th>
+							<th scope="col">Correo</th>
+							<th scope="col">Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="">
+							<td scope="row">R1C1</td>
+							<td>ivan</td>
+							<td>lopesaguilar14@gmail.com</td>
+							<td>Editar | Borrar </td>
+						</tr>
+
+					</tbody>
+				</table>
+			</div>
